f830fcbad2ae08a49732631ca15d0b4a9fad0770
Triusdeil <triusdeil@gmail.com>
2021-11-11 23:38:24 -0400

    iniciando el backend

---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
+import { options } from "./swaggerOptions";
+
+const specs = swaggerJSDoc(options);
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(specs));
+export default app;
---
+++ backend/src/swaggerOptions.js
@@ -0,0 +1,8 @@
+export const options = {
+  definition: {
+    info: {
+      title: "Mern Whatsapp",
+    },
+  },
+  apis: ["./src/routes/**/*.js"],
+};
