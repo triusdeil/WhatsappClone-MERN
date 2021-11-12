c735df9786ce8e756f6d081c9bfff907c47084c9
Triusdeil <triusdeil@gmail.com>
2021-11-11 22:35:06 -0400

    frontend

--- frontend/src/components/chat/Chat.js
+++ frontend/src/components/chat/Chat.js
@@ -1,9 +1,65 @@
 import React from "react";
 import "../../css/Chat.css";
+import { Avatar, IconButton } from "@mui/material";
+import {
+  SearchOutlined,
+  AttachFile,
+  MoreVert,
+  InsertEmoticon,
+  Mic,
+} from "@mui/icons-material";
 const Chat = () => {
   return (
     <div className="chat">
-      <h1>Chat</h1>
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at ...</p>
+        </div>
+        <div className="chat_headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Luis</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Triusdeil</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message">
+          <span className="chat__name">Luis</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <IconButton>
+          <InsertEmoticon />
+        </IconButton>
+        <form>
+          <input type="text" placeholder="Type a message" />
+          <button type="submit">Send a Message</button>
+        </form>
+        <IconButton>
+          <Mic />
+        </IconButton>
+      </div>
     </div>
   );
 };
